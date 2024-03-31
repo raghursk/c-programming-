a7ca82e79934d07614e20d343ab4c36aa90c952f
raghursk <108519569+raghursk@users.noreply.github.com>
2024-03-31 12:32:15 +0530

    Add files via upload

---
+++ math.h.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+// int main()
+// {
+//     int x;
+//     printf("enter the x value:\n");
+//     scanf("%d",&x);
+//     int y;
+//     y=sqrt(x); //================sqrt function
+//     printf("%d",y);
+//}
+// int main()
+// {
+//     int x=2;
+//     int r;
+//     r=pow(x,2+1)+(x,3+1);//===========================================power function 
+//     printf("%d",r);
+    
+// }
+// #include <stdio.h>
+// #include <math.h>
+
+// int main() {
+//     int x, y;
+//     printf("Enter the value of x: ");// enter the value only int datatype
+//     scanf("%d", &x);
+
+//     int z = floor(x); // Calculate the floor of x and store it in z
+
+//     printf("Enter the value of y: ");
+//     scanf("%d", &y);
+
+//     int result = z + y;
+
+//     printf("The result is: %d\n", result);
+
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int x=1.9;
+//     int y=ceil(x);//
+//     printf("%d",y);
+// }
+// int main()
+// {
+//     int x;
+//     printf("enter the x value:\n");//==========+ve to -ve and -ve to +ve
+//     scanf("%d",&x);
+//     int y=abs(x);
+//     printf("%d",y);
+// }
+// int main()
+// {
+//       int x;
+    // printf("enter the x value:\n");//==========round the numbers 8.15=8
+    // scanf("%d",&x);
+//     int y= round(x);
+//     printf("%d",y);
+// }
+
